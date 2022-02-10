dfb66232c77acd8cebca3266713e41a03838ce58
zxm1124 <34598821+zxm1124@users.noreply.github.com>
2022-02-10 13:08:27 +0800

    Update controller.go

--- api/authserver/api/parse/controller/v1/controller.go
+++ api/authserver/api/parse/controller/v1/controller.go
@@ -4,7 +4,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	auth "github.com/zxm1124/component-base/pkg/auth/v1"
 	"net/http"
-	"store-ming/api/authserver/api/parse/global"
+	"github.com/zxm1124/store-ming/api/authserver/api/parse/global"
 )
 
 // OnAuth 校验用户token
